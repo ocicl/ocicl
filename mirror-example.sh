a932f6513108018942fd721676cdab148f6c81da
Anthony Green <green@moxielogic.com>
2023-06-09 08:46:57 -0400

    Fix script

--- mirror-example.sh
+++ mirror-example.sh
@@ -7,21 +7,24 @@
 # Once you've created your mirror, configure ocicl with the --registry
 # option.
 
-project=""
+system=""
 
-while IFS= read -r line; do
-  # Check if the line is empty or contains a colon (indicating a new project)
-  if [[ -z $line || $line == *":" ]]; then
-    # If line contains a colon, strip it and assign to project
-    if [[ $line == *":" ]]; then
-      project="${line%:}"
-    else
-      project=""
-    fi
-  # If line contains a version tag
-  elif [[ -n $project ]]; then
-    version=$(echo $line | awk '{ print $1 }')
-    echo "Running command on $project:$version"
-    skopeo copy --dest-tls-verify=false  docker://ghcr.io/ocicl/${project}:${version} docker://localhost:8080/ocicl/${project}:${version}
-  fi
-done < file-with-list-of-packages.txt
+for system in $(curl -S https://raw.githubusercontent.com/ocicl/request-system-additions-here/main/all-ocicl-systems.txt); do
+    ocicl list $system | while IFS= read -r line; do
+        # Check if the line is empty or contains a colon (indicating a new project)
+        if [[ -z $line || $line == *":" ]]; then
+            # If line contains a colon, strip it and assign to project
+            if [[ $line == *":" ]]; then
+                system="${line%:}"
+            else
+                system=""
+            fi
+            # If line contains a version tag
+        elif [[ -n $system ]]; then
+            version=$(echo $line | awk '{ print $1 }')
+            echo "Copying system and signature for $system:$version"
+            skopeo copy --dest-tls-verify=false  docker://ghcr.io/ocicl/${system}:${version} docker://localhost:8080/ocicl/${system}:${version}
+            skopeo copy --dest-tls-verify=false  docker://ghcr.io/ocicl/${system}.sig:${version} docker://localhost:8080/ocicl/${system}.sig:${version}
+        fi
+    done
+done
