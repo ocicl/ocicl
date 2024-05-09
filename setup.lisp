4115aa92bcfce2f4f35fbac7c0b8aaa5eb3e00bc
Anthony Green <green@moxielogic.com>
2024-05-09 18:09:45 -0400

    `chore(setup.lisp): update install-oras format string`

--- setup.lisp
+++ setup.lisp
@@ -97,7 +97,7 @@
        (format t "~%~%Be sure to add ~A\\AppData\\Local\\ocicl\\ to your path!~%~%"
                (uiop:getenv "UserProfile")))
       ((uiop:os-unix-p)
-       (install-oras (format nil "oras_1.1.0_linux_~A.tar.gz" arch)))))
+       (install-oras (format nil "oras_1.1.0_linux_~A.tar.gz" arch))))))
 
 
 (make-ocicl)
