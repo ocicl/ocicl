45d14dffd5e91a1a22ccbfbb6192312df366a949
Anthony Green <green@moxielogic.com>
2024-11-13 05:49:17 -0500

    `fix(ocicl): improve error logging by adding backtrace`

--- ocicl.lisp
+++ ocicl.lisp
@@ -757,7 +757,7 @@ Distributed under the terms of the MIT License"
                                                   (setf (gethash (mangle (pathname-name s)) *ocicl-systems*) (cons #?"${registry}/${mangled-name}@${manifest-digest}"
                                                                                                                    (subseq (namestring s) (length (namestring *systems-dir*))))))))
                                             (return t))
-                                        (error (e) (debug-log e))))
+                                        (error (e) (debug-log e) (sb-debug:print-backtrace))))
                        (format t "; error downloading ~A~%" name))))
               (uiop:delete-directory-tree dl-dir :validate t)))
           (write-systems-csv)
